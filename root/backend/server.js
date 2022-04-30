9ac7d905686492863914eb2e84a7e80aa33b252f
Steeve Johan Otoka Eyota <73400373+johanotoka@users.noreply.github.com>
2022-04-30 13:14:35 -0400

    clean up

--- root/backend/server.js
+++ root/backend/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./connection");
 
-// const path = require('path');
-// require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require("./routes/events");
